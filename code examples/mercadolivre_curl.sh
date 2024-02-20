d7fc52bcdea98d86d041ed288e635dd5376a5883
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:40:37 +0200

    Adding code examples/mercadolivre_curl.sh

---
+++ code examples/mercadolivre_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://lista.mercadolivre.com.br/eletronicos-audio-video/_loja_mercado-livre#deal_print_id=85ade3d0-cfda-11ee-8f23-2701100dea38&c_id=special-normal&c_element_order=4&c_campaign=eletronicos&c_uid=85ade3d0-cfda-11ee-8f23-2701100dea38"}'
